63d62b5cddb423cf3d728213eb6d0088d484e64e
MikeRock51 <mikerockmusic51@gmail.com>
2024-07-23 05:03:16 +0100

    chore: implement signin on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,28 @@
 const UserModel = require('../models/userModel');
 const AppError = require('../utils/errors/AppErrors');
+const jwt = require('jsonwebtoken');
 
 exports.signup = AppError.catchAsync(async (req, res, next) => {
-    const newUser = await UserModel.create(req.body);
+    const newUser = await UserModel.create(
+        {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirmed: req.body.passwordConfirmed,
+        }
+    );
+
+    const token = jwt.sign(
+        { id: newUser._id },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_TTL }
+    );
 
     res.status(201).json({
         status: 'success',
         message: 'User created successfully!',
         data: {
+            token,
             user: newUser
         },
     });
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = mongoose.Schema({
     name: {
@@ -20,7 +21,7 @@ const UserSchema = mongoose.Schema({
     },
     passwordConfirmed: {
         type: String,
-        required: [true, 'Please provide a password...'],
+        required: [true, 'Please confirm your password...'],
         min: 8,
         validate: {
             validator: function(el) {
@@ -31,6 +32,15 @@ const UserSchema = mongoose.Schema({
     },
     photo: String,
 });
+
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirmed = undefined;
+
+    next();
+})
 
 const UserModel = mongoose.model('User', UserSchema);
 
